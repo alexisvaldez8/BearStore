eb18cefa0fc61018805f55de8b5b634f2f3f0c8d
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2019-05-07 20:56:47 -0500

    menu funcionando al 100%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,55 +21,64 @@ export class AppComponent implements OnInit {
 
   public ngOnInit(){
 
+		$(".submenu").click(function(){
+			$(this).children("ul").slideToggle();
+		})
+
     var slider = $('#slider');
-//almacenar botones
-var siguiente = $('#btn-next');
-var anterior = $('#btn-prev');
+		//almacenar botones
+		var siguiente = $('#btn-next');
+		var anterior = $('#btn-prev');
 
-//mover ultima imagen al primer lugar
-$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
-//mostrar la primera imagen con un margen de -100%
-slider.css('margin-left', '-'+100+'%');
-
-function moverD() {
-	slider.animate({
-		marginLeft:'-'+200+'%'
-	} ,700, function(){
-		$('#slider .slider__section:first').insertAfter('#slider .slider__section:last');
-		slider.css('margin-left', '-'+100+'%');
-	});
-}
-
-function moverI() {
-	slider.animate({
-		marginLeft:0
-	} ,700, function(){
+		//mover ultima imagen al primer lugar
 		$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+		//mostrar la primera imagen con un margen de -100%
 		slider.css('margin-left', '-'+100+'%');
-	});
-}
 
-function autoplay() {
-	this.interval = setInterval(function(){
-		moverD();
-	}, 5000);
-}
-siguiente.on('click',function() {
-	moverD();
-	clearInterval(this.interval);
-	autoplay();
-});
+		function moverD() {
+			slider.animate({
+				marginLeft:'-'+200+'%'
+			} ,700, function(){
+				$('#slider .slider__section:first').insertAfter('#slider .slider__section:last');
+				slider.css('margin-left', '-'+100+'%');
+			});
+		}
 
-anterior.on('click',function() {
-	moverI();
-	clearInterval(this.interval);
-	autoplay();
-});
+		function moverI() {
+			slider.animate({
+				marginLeft:0
+			} ,700, function(){
+				$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+				slider.css('margin-left', '-'+100+'%');
+			});
+		}
+
+		function autoplay() {
+			this.interval = setInterval(function(){
+				moverD();
+			}, 5000);
+		}
+		siguiente.on('click',function() {
+			moverD();
+			clearInterval(this.interval);
+			autoplay();
+		});
+
+		anterior.on('click',function() {
+			moverI();
+			clearInterval(this.interval);
+			autoplay();
+		});
 
 
-autoplay();
+		autoplay();
 
-  }
+
+
+
+		
+
+  		}
 
 
 
