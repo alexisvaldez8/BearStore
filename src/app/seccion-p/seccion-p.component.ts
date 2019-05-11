a186c030f9fa5bf593483c3934e636f5b2117f98
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2019-05-10 22:31:51 -0500

    pagina de seccion

--- src/app/seccion-p/seccion-p.component.ts
+++ src/app/seccion-p/seccion-p.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeccionPComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
+    seccion:any;
+    genero:any;
+    titulo;any;
 
+  
   ngOnInit() {
+    this.seccion="Playeras"
+  
+  this.genero="Hombre";
+
+  this.titulo=this.seccion+" para "+this.genero;
+    function recuperarSeccion() {
+      //aqui vamos a recuperar el genero y categoria a mostrar
+      }
+    function recuperarGenero() {
+    //aqui vamos a recuperar el genero y categoria a mostrar
+    }
   }
 
 }
