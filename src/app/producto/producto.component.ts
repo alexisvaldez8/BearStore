bbd48e0e47748703eaddfce57e7c9065f14a43c6
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2019-05-12 23:04:51 -0500

    pagina de producto a un 90

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -9,6 +9,12 @@ export class ProductoComponent implements OnInit {
 
   constructor() { }
 
+  nombreProducto:String;
+  precioProducto="$269.00";
+
+  descripcionP="• Playera manga corta, cuello redondo • Estampado en serigrafía sin tacto • 50% algodón 50% poliéster  • Hecho en México"
+
+  
   ngOnInit() {
   }
 
