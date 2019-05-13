da0ca8de557fa3ed33f33947403412ff227a5a9d
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2019-05-13 11:44:43 -0500

    ngfor

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -13,6 +13,30 @@ export class PedidosComponent implements OnInit {
 
   titulo="Mis Pedidos";
 
+  pedidos:[
+    {
+      idPedido:'1',
+      fecha:'2019-05-10',
+      totalPagado:'269.00'
+    },
+    {
+      idPedido:'2',
+      fecha:'2019-05-11',
+      totalPagado:'538.00'
+    },
+    {
+      idPedido:'3',
+      fecha:'2019-05-12',
+      totalPagado:'807.00'
+    }
+
+  ];
+
+  muestrapedidos(){
+  console.log(this.pedidos);
+  }
+
+
   ngOnInit() {
     
 
