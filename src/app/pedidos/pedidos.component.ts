d93e34ba89cbcbfee135eeeb9a59283a586a8a64
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2019-05-15 22:11:15 -0500

    servicio que regresa pedidos listo!

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import {ConsultasService} from '../consultas.service';
+
 
 
 @Component({
@@ -8,20 +10,41 @@ import * as $ from 'jquery';
   styleUrls: ['./pedidos.component.css']
 })
 export class PedidosComponent implements OnInit {
-  constructor() { }
+  
+  constructor(public http:ConsultasService) {
+    //this.muestrapedidos();
+    this.traerPedidos();
+  }
 
   titulo="Mis Pedidos";
 
+  pedidos;
   
-  pedidos=[{
-     id:'1',fecha:'2019-05-10',totalPagado:'269.00'},
-    {id:'2',fecha:'2019-05-11',totalPagado:'360.00'},
-    {id:'3',fecha:'2019-05-12',totalPagado:'570.00'}
-  ];
+  /*pedidos=[{
+     idpedido:'1',fecha:'2019-05-10',total:'269.00'},
+    {idpedido:'2',fecha:'2019-05-11',total:'360.00'},
+    {idpedido:'3',fecha:'2019-05-12',total:'570.00'}
+  ];*/
 
   
     muestrapedidos(){
       console.log(this.pedidos);
+      }
+
+
+      traerPedidos(){
+        this.http.regresaPedidos().then(
+          (data)=>{
+            console.log("imprime data...");
+            console.log(data);
+            this.pedidos=data;
+            this.pedidos=this.pedidos.pedidos;
+            console.log("llenado...");
+            console.log(this.pedidos);
+          },(error)=>{
+            console.log("ERROR "+JSON.stringify(error));
+          }
+        );
       }
 
   ngOnInit() {
